63375651469eb68bbd80b0e98effcb729fcd0e7c
jarrizabalaga <j.arrizabalaga1@gmail.com>
2016-02-04 13:14:11 +0100

    Conexión a base de datos postgres

--- DjangoTest/settings.py
+++ DjangoTest/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'DjangoTest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangotest',
+        'USER': 'fjarrizabalaga',
+        'PASSWORD': 'S1mps0nS',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
